2103e4b9819d9f0965a585a11867b0ec94060ee6
Dhanushkumar <130890976+M-S-Dhanushkumar@users.noreply.github.com>
2023-09-20 13:48:37 +0530

    Update SAS to XLSX(Single Dataset).py
    Added convert bytes to string.

--- SAS to XLSX(Single Dataset).py
+++ SAS to XLSX(Single Dataset).py
@@ -1,13 +1,16 @@
 import pandas as pd
 
 # Specify the path to the SAS dataset file (.sas7bdat)
-sas_dataset_path = '/path/to/your/dataset.sas7bdat'
+sas_dataset_path = '/content/input/ab.sas7bdat'
 
 # Specify the output Excel file name (with .xlsx extension)
-output_excel_file = '/path/to/your/output_dataset.xlsx'
+output_excel_file = '/content/Output/ab.xlsx'
 
 # Read the SAS dataset using sas7bdat
 sas_data = pd.read_sas(sas_dataset_path, format='sas7bdat')
+
+# Convert bytes to string data by decoding
+sas_data = sas_data.applymap(lambda x: x.decode('utf-8') if isinstance(x, bytes) else x)
 
 # Write the data to the Excel file
 sas_data.to_excel(output_excel_file, index=False)
